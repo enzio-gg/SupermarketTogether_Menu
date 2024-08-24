d269bcd549a4cf20cfc02b58bb6d74b594a273a7
Steffen <github@projectenzo.io>
2024-08-25 00:13:54 +0200

    Added: New settings

--- ENZIO_SupermarketTogetherMenu/Settings.cs
+++ ENZIO_SupermarketTogetherMenu/Settings.cs
@@ -8,15 +8,21 @@
 
         public static bool duping = true;
         public static int dupingAmount = 10;
-        public static float employeeItemPlaceWait = 0.01f;
-        public static float productCheckoutWait = 0.01f;
-        public static float productItemPlaceWait = 0.01f;
-        public static float speed = 5f;
-        public static float acceleration = 50f;
-        public static float angularSpeed = 1000f;
-        public static float extraEmployeeSpeedFactor = 0.5f;
+
+        public static float playerMoveSpeed = 10f;
+        public static float playerSprintSpeed = 15f;
+
+        public static bool editEmployeeSpeed = true;
         public static int maxEmployees = 10;
-        public static float moveSpeed = 10f;
-        public static float sprintSpeed = 15f;
+        public static float employeeItemPlaceWait = 0.01f;
+        public static float extraEmployeeSpeedFactor = 0.5f;
+
+        public static bool editCustomerSpeed = true;
+        public static bool editNpcSpeed = false;
+        public static float npcProductCheckoutWait = 0.01f;
+        public static float npcProductItemPlaceWait = 0.01f;
+        public static float npcSpeed = 10f;
+        public static float npcAcceleration = 50f;
+        public static float npcAngularSpeed = 1000f;
     }
 }
