ebe313f940f131e898bfc23fe08d2073d2b140dc
Steffen <github@projectenzo.io>
2024-08-25 00:50:48 +0200

    Added: Helpers class Added: Main scene check method

---
+++ ENZIO_SupermarketTogetherMenu/Helpers.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace ENZIO
+{
+    internal class Helpers
+    {
+        internal static bool IsMainSceneLoaded()
+        {
+            return SceneManager.GetActiveScene().buildIndex == 1 && SceneManager.GetActiveScene().name == "B_Main" && !SceneManager.GetActiveScene().isLoaded;
+        }
+    }
+}
